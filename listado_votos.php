beb48a5711c9616a6443b1d3a4015efcf8287100
noelia0401 <noelia.alvvic@educa.jcyl.es>
2021-11-10 13:05:43 +0100

    sdfshf

--- listado_votos.php
+++ listado_votos.php
@@ -1,7 +1,21 @@
 <?php
 session_start();
 include 'head.php';
-                                             
+
+echo '<table>
+    <thead>
+        <th>Codigo tapa</th>
+        <th>numero votos</th>
+    </thead>
+    <tbody>';
+    foreach ($_SESSION['votos'] as $clave=>$valor)
+    {
+    echo '<tr>
+        <td>'.$clave.'</td>
+        <td>'.$valor.'</td>
+    </tr>';
+    }
+    echo '</tbody>';
  
  include 'pie.php';
 
--- votar_tapa.php
+++ votar_tapa.php
@@ -1,23 +1,32 @@
 <?php
 session_start();
 include 'head.php';
-                                             
-echo'<form action="" method="post">
-            Selecciona la Tapa a la cual quieres votar
-                            <select name="codigo">
-                                  <option value="">Crujiente de Arroz con rollitos de atún y verduras en escabeche</option>
-                                  <option value="">La Mar de Pincho</option>
-                                  <option value="">Pincho el indiano de Cantabria</option>
-                                  <option value="">Tierra, sabor, tradición</option>
-                                  <option value="">Txerri Beltz</option>
-                                  <option value="">Esencia</option>
-                                  <option value="">Rebanada de Vida</option>
-                                 
-                                </select>
-     <br> 
-     <input name="votar" type="submit"  value="Votar"/>
+
+if (isset($_REQUEST['votar']))
+{
+  $codigo=$_REQUEST['codigo'];
+  $_SESSION['votos'][$codigo]++;
+}
+
+echo'<form action="votar_tapa.php" method="post">
+  Selecciona la Tapa a la cual quieres votar
+    <select name="codigo">';
+    foreach ($_SESSION['tapas'] as $clave=>$valor)
+    {
+    echo '<option value="'.$clave.'">'.$valor[0].'<option>';
+    }
+    //<option value="">Crujiente de Arroz con rollitos de atún y verduras en escabeche</option>
+    //<option value="">La Mar de Pincho</option>
+    //<option value="">Pincho el indiano de Cantabria</option>
+    //<option value="">Tierra, sabor, tradición</option>
+    //<option value="">Txerri Beltz</option>
+    //<option value="">Esencia</option>
+    //<option value="">Rebanada de Vida</option>
+    echo '</select>
+    <br> 
+<input name="votar" type="submit"  value="Votar"/>
              
-    </form>';
+</form>';
 
  include 'pie.php';
 
